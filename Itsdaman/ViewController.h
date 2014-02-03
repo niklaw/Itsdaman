cea68462c657f26fee77bf968fd6d509c7566789
Yash Varma <vyash8@gmail.com>
2014-02-03 15:11:15 -0600

    Initial Commit

---
+++ Itsdaman/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Itsdaman
+//
+//  Created by Yash Varma on 2/3/14.
+//  Copyright (c) 2014 Yash Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Itsdaman/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Itsdaman
+//
+//  Created by Yash Varma on 2/3/14.
+//  Copyright (c) 2014 Yash Varma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
